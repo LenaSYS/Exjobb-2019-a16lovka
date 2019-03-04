8eb4081f98d0053a9519aa59d1589df58cef2c8c
Lovisa <a16lovka@student.his.se>
2019-03-04 09:16:53 +0100

    update syntax changes

--- database.sql
+++ database.sql
@@ -7,54 +7,54 @@ CREATE TABLE JOSN_Tiny(
 	Id INT AUTO_INCREMENT,
     JOSN_Tiny_Data JSON,
     PRIMARY KEY (Id)
-)ENGINE=INNODB;
+)ENGINE=InnoDB;
 
 /* Table for datatype JSON with same data size as BLOB */
 CREATE TABLE JSON_Normal(
-	Id INT auto_increment,
+	Id INT AUTO_INCREMENT,
     JSON_Normal_Data JSON,
     PRIMARY KEY (Id)
-)ENGINE=INNODB;
+)ENGINE=InnoDB;
 
 /* Table for datatype JSON with same data size as MEDIUMBLOB */
 CREATE TABLE JSON_Medium(
-	Id INT auto_increment,
+	Id INT AUTO_INCREMENT,
     JSON_Medium_Data JSON,
     PRIMARY KEY (Id)
-)ENGINE=INNODB;
+)ENGINE=InnoDB;
 
 /* Table for datatype JSON with same data size as LONGBLOB */
 CREATE TABLE JSON_Long(
-	Id INT auto_increment,
+	Id INT AUTO_INCREMENT,
     JSON_Long_Data JSON,
     PRIMARY KEY (Id)
-)ENGINE=INNODB;
+)ENGINE=InnoDB;
 
 /* Table for datatype TINYBLOB */
 CREATE TABLE BLOB_Tiny(
-	Id INT auto_increment,
+	Id INT AUTO_INCREMENT,
     BLOB_Tiny_Data TINYBLOB,
     PRIMARY KEY (Id)
-)ENGINE=INNODB;
+)ENGINE=InnoDB;
 
 /* Table for datatype BLOB */
 CREATE TABLE BLOB_Normal(
-	Id INT auto_increment,
+	Id INT AUTO_INCREMENT,
     BLOB_Normal_Data BLOB,
     PRIMARY KEY (Id)
-)ENGINE=INNODB;
+)ENGINE=InnoDB;
 
 /* Table for datatype MEDIUMBLOB */
-CREATE TABLE BLOB_M(
-	Id INT auto_increment,
+CREATE TABLE BLOB_Medium(
+	Id INT AUTO_INCREMENT,
     BLOB_Medium_Data MEDIUMBLOB,
     PRIMARY KEY (Id)
-)ENGINE=INNODB;
+)ENGINE=InnoDB;
 
 /* Table for datatype LONGBLOB */
 CREATE TABLE BLOB_Long(
-	Id INT auto_increment,
+	Id INT AUTO_INCREMENT,
     BLOB_Long_Data LONGBLOB,
     PRIMARY KEY (Id)
-)ENGINE=INNODB;
+)ENGINE=InnoDB;
 
