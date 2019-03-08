202d4efbf28abc0ac5255dac0a52c9246c7afe9a
a16lovka <a16lovka@student.his.se>
2019-03-08 14:53:51 +0100

    setup database connection

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+
+$server = "127.0.0.1";
+$username = "root";
+$password = "password";
+$database = "Products";
+
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+?>
