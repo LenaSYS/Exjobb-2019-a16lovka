58c75138b83391d93b5ec2c7dc52ca063a6287fe
a16lovka <a16lovka@student.his.se>
2019-03-12 11:51:29 +0100

    run the insert files

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    require('connection.php');
+
+    //Inserts data to tiny-data tables
+    require('insert_data_tiny.php');
+
+    //Inserts data to normal-data tables
+    require('insert_data_normal.php');
+
+    //Inserts data to medium-data tables
+    require('insert_data_medium.php');
+    
+    //Inserts data to long-data tables
+    require('insert_data_long.php');
+
+?>
