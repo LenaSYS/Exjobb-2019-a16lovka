c3bcd297bea6b66aba0739aba24cdee411263cd4
a16lovka <a16lovka@student.his.se>
2019-03-20 11:03:16 +0100

    run the insert and select files

--- index.php
+++ index.php
@@ -2,15 +2,19 @@
     require('connection.php');
 
     //Inserts data to tiny-data tables
-    require('insert_data_tiny.php');
+    /* require('insert_data_tiny.php'); */
 
     //Inserts data to normal-data tables
-    require('insert_data_normal.php');
+    /* require('insert_data_normal.php'); */
 
     //Inserts data to medium-data tables
-    require('insert_data_medium.php');
+    /* require('insert_data_medium.php'); */
     
     //Inserts data to long-data tables
-    require('insert_data_long.php');
+    /* require('insert_data_long.php');  */
+
+    /* require('select_data_blob.php'); */
+    /* require('select_data_text.php'); */
+    require('select_data_json.php');
 
 ?>
