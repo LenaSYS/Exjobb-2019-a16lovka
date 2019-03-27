bd3c91920409808c16bdb04a8654d0c588da79b8
a16lovka <a16lovka@student.his.se>
2019-03-27 11:06:16 +0100

    remove loops

--- insert_data_normal.php
+++ insert_data_normal.php
@@ -8,16 +8,13 @@ $data= json_decode($data);
 
 try {
 
-    for ($i=0; $i < sizeof($data); $i++) { 
+    $dataToInsert = json_encode($data);
 
-        $dataToInsert = json_encode($data[$i]);
+    $sql = "INSERT INTO JSON_Normal(JSON_Normal_Data) VALUES('$dataToInsert');
+            INSERT INTO BLOB_Normal(BLOB_Normal_Data) VALUES('$dataToInsert'); 
+            INSERT INTO TEXT_Normal(TEXT_Normal_Data) VALUES('$dataToInsert');";
 
-        $sql = "INSERT INTO BLOB_Normal(BLOB_Normal_Data) VALUES('$dataToInsert');
-                INSERT INTO JSON_Normal(JSON_Normal_Data) VALUES('$dataToInsert');
-                INSERT INTO TEXT_Normal(TEXT_Normal_Data) VALUES('$dataToInsert')";
-
-        $conn->exec($sql);
-    }
+    $conn->exec($sql);
     
     echo "New records created successfully";
 }
