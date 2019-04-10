366efe2ca8b1e4f88e828cfb3d119ead2ab37dee
a16lovka <a16lovka@student.his.se>
2019-04-10 11:47:56 +0200

    add php file for second the select query for json datatype

---
+++ select_data_json2.php
@@ -0,0 +1,35 @@
+<pre>
+<?php
+
+    ini_set('memory_limit', '5000M');
+    ini_set('max_execution_time', 240);
+
+    $stmt = $conn->prepare("SELECT JSON_Tiny_Data FROM JSON_Tiny");
+
+    $time_start = microtime(true);
+    $stmt->execute();
+    $data = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+
+    $item = json_decode($data[0]);
+
+    for ($i=0; $i < sizeof($item); $i++) {   
+        $obj = $item[$i];
+        if($obj->{'price'} < 500) {
+            print_r($obj->{'id'} . "\n");
+        } 
+    }
+
+    $time_end = microtime(true);
+   
+    $time = $time_end - $time_start;
+
+    $file = '../responseTimes.txt';
+    $current = file_get_contents($file);
+    $current .= $time." \n";
+    file_put_contents($file, $current);
+
+?>
+</pre>
+<script src="reload_count.js"></script>
+
+
