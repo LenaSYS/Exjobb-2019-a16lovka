fe1a2839e21495a5dc39bf512a3bbc28ebde1bad
a16lovka <a16lovka@student.his.se>
2019-04-16 10:57:17 +0200

    files for update queries

---
+++ update_data_text.php
@@ -0,0 +1,42 @@
+<pre>
+<?php
+
+    ini_set('memory_limit', '5000M');
+    ini_set('max_execution_time', 240);
+
+    $stmt = $conn->prepare("SELECT TEXT_Normal_Data FROM TEXT_Normal");
+
+    $time_start = microtime(true);
+    $stmt->execute();
+
+    $data = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+    $data = json_decode($data[0]);
+
+    for ($i=0; $i < sizeof($data); $i++) {   
+        
+        $obj = $data[$i];
+        $pCategory = $obj->{'categories'}[0];
+        
+        if($pCategory->{'parent_category'} == 'Hats') {
+            $obj->{'price'} = $obj->{'price'} + 1;
+        }
+    }
+
+
+    $dataToInsert = json_encode($data);
+    $sql = "UPDATE TEXT_Normal SET TEXT_Normal_Data = '" . $dataToInsert . "'";
+    $conn->exec($sql);
+
+    $time_end = microtime(true);
+    $time = $time_end - $time_start;
+
+    $file = '../responseTimes.txt';
+    $current = file_get_contents($file);
+    $current .= $time." \n";
+    file_put_contents($file, $current);
+
+?>
+</pre>
+<script src="reload_count.js"></script>
+
+
